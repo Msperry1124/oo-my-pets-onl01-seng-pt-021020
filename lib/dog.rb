dcf8d659f016540e650c995680f12aa0752a8048
Melissa Sperry <sperry.mel@gmail.com>
2020-03-14 04:37:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,8 +3,9 @@ class Dog
   attr_accessor :owner, :mood
   attr_reader :name
 
-  def initialize(name)
+  def initialize(name, owner, mood = "nervous")
     @name = name
-    @mood = "nervous"
+    @owner = owner
+    @mood = mood
   end
 end
